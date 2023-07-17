2256ed3dc2f7644ea7d23cf2938f163675d13abd
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2023-07-18 00:26:16 +0300

    modified

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var openTab = document.querySelector("#check_btn"),
+var openTab = document.querySelector(".check_btn"),
     nav = document.querySelector(".responsive_list"),
-    closeTab = document.querySelector("#closing_btn") 
+    closeTab = document.querySelector(".closing_btn") 
 openTab.onclick = function(){
     nav.classList.add("open")
 }
